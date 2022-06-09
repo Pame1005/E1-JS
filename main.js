ca40f16684600eb9fcd849c0c5088c3d715d68f3
Pamela <pamelaeugeniamartin@gmail.com>
2022-06-09 08:07:13 -0300

    Últimos cambios

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ const ingredientes = ["Harina", "Queso", "Levadura", "Aceite", "Agua", "jamón",
 const par = [];
 const impar = [];
 
-function pizza(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
-        if (array[i].length % 2 === 0) {
-            par.push(array[i]);
+function pizza(ingredientes) {
+    for (let i = 0; i < ingredientes.length; i++) {
+        console.log(ingredientes[i]);
+        if (ingredientes[i].length % 2 === 0) {
+            par.push(ingredientes[i]);
         }
         else {
-            impar.push(array[i]);
+            impar.push(ingredientes[i]);
         }
     }
 }
 
 pizza (ingredientes);
-
 console.log(`Las palabras pares son: ${par}`);
 console.log(`Las palabras impares son: ${impar}`);
